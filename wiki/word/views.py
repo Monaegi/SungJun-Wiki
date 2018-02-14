10aab0c6e1793fe51e5ceda940a637c87f612029
darkblank <darkblank1990@gmail.com>
2018-02-14 21:11:46 +0900

    camel words a link 걸어줌

--- wiki/word/views.py
+++ wiki/word/views.py
@@ -1,3 +1,5 @@
+import re
+
 from django.http import Http404
 from django.shortcuts import redirect, render, get_object_or_404
 
@@ -30,6 +32,7 @@ def wiki_detail(request, title):
         title_list = [x.title for x in queryset]
         text = instance.text
         text_to_list = text.replace('\r\n', ' ').split(' ')
+        camel_list = re.findall(r'^[A-Z]\w*?[A-Z]\w*?$', text)
 
         for title in title_list:
             if title in text_to_list:
@@ -40,6 +43,11 @@ def wiki_detail(request, title):
                 text = text.replace(value, f'<a href="{value}">{value}</a>')
             elif value.startswith('[') and value.endswith(']'):
                 text = text.replace(value, f'<a href="/wiki/detail/{value[1:-1]}">{value[1:-1]}</a>')
+
+        for camel in camel_list:
+            if camel in text_to_list:
+                text = text.replace(camel, f'<a href="/wiki/detail/{camel}">{camel}</a>')
+
         context = {
             'instance': instance,
             'text': text,
