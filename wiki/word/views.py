b16a37ad3dcfedfe93cb61f306dc70b904ed80ac
darkblank <darkblank1990@gmail.com>
2018-02-14 22:51:24 +0900

    코드의 간단한 설명 추가

--- wiki/word/views.py
+++ wiki/word/views.py
@@ -8,6 +8,7 @@ from word.models import WikiWord
 
 
 def wiki_create(request, title):
+    # 생성하려는 단어가 이미 데이터베이스에 있는 경우 해당 단어의 detail url로 이동
     if WikiWord.objects.filter(title=title).exists():
         return redirect('wiki-detail', title=title)
 
@@ -37,10 +38,14 @@ def wiki_detail(request, title):
         camel_list = re.findall(r'^[A-Z]\w*?[A-Z]\w*?$', text)
 
         for item in text_to_list:
+            # 본문의 단어가 특정 Title과 일치하거나 Camel 단어이면 링크 걸림
+            # Camel의 정의 : 맨 첫 글자가 대문자이고, 단어 중간에 대문자가 한번 이상 더 들어가는 단어
             if item.lower() in title_list or item in camel_list:
                 text = text.replace(item, f'<a href="/wiki/detail/{item}">{item}</a>')
+            # http:// 또는 https://로 시작하는 단어도 링크 걸어 줌
             elif item.startswith('http://') or item.startswith('https://'):
                 text = text.replace(item, f'<a href="{item}">{item}</a>')
+            # [로 시작하고 ]로 끝나는 단어는 대괄호를 없애서 본문에 나타나도록 하며 링크를 걸어 줌
             elif item.startswith('[') and item.endswith(']'):
                 text = text.replace(item, f'<a href="/wiki/detail/{item[1:-1]}">{item[1:-1]}</a>')
 
@@ -49,6 +54,6 @@ def wiki_detail(request, title):
             'text': text,
         }
         return render(request, 'wiki_detail.html', context)
-
+    # 데이터베이스에 있지 않은 단어의 detail url 입력 시 단어 생성 Form 으로 이동
     except Http404:
         return redirect('wiki-create', title=title)
