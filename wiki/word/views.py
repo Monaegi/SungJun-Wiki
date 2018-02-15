97db2361996de2e37b7899cd0a03ffeb40b67517
darkblank <darkblank1990@gmail.com>
2018-02-15 20:14:49 +0900

    기준이 되는 instance와 내용이 50%이상 일치하는 단어들을 연관검색어로 보여주도록 구현

--- wiki/word/views.py
+++ wiki/word/views.py
@@ -36,6 +36,8 @@ def wiki_detail(request, title):
         text = instance.text
         text_to_list = text.replace('\r\n', ' ').split(' ')
         camel_list = re.findall(r'^[A-Z]\w*?[A-Z]\w*?$', text)
+        # similar() = WikiWord(Model) instance method
+        relevant_instances = instance.similar()
 
         for item in text_to_list:
             # 본문의 단어가 특정 Title과 일치하거나 Camel 단어이면 링크 걸림
@@ -45,13 +47,14 @@ def wiki_detail(request, title):
             # http:// 또는 https://로 시작하는 단어도 링크 걸어 줌
             elif item.startswith('http://') or item.startswith('https://'):
                 text = text.replace(item, f'<a href="{item}">{item}</a>')
-            # [로 시작하고 ]로 끝나는 단어는 대괄호를 없애서 본문에 나타나도록 하며 링크를 걸어 줌
+            # [로 시작하고 ]로 끝나는 단어는 대괄호를 삭제하여 본문에 나타나도록 하며 링크를 걸어 줌
             elif item.startswith('[') and item.endswith(']'):
                 text = text.replace(item, f'<a href="/wiki/detail/{item[1:-1]}">{item[1:-1]}</a>')
 
         context = {
             'instance': instance,
             'text': text,
+            'relevant_instances': relevant_instances,
         }
         return render(request, 'wiki_detail.html', context)
     # 데이터베이스에 있지 않은 단어의 detail url 입력 시 단어 생성 Form 으로 이동
